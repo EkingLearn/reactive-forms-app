ac1711785fe75549314c584532bec085fe3c3a8c
Erick Barahona <erfebaro@gmail.com>
2025-08-07 10:28:14 -0600

    add initial implementation

---
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,26 @@
+import { JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-basic-page',
+  imports: [JsonPipe, ReactiveFormsModule],
+  templateUrl: './basic-page.component.html',
+})
+export class BasicPageComponent {
+  // basicForm = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // })
+
+  private fb = inject(FormBuilder);
+
+  basicForm = this.fb.group({
+    name:['',[Validators.required, Validators.minLength(3)]],
+    price:[0,[Validators.required, Validators.min(10)]],
+    inStorage:[0, [Validators.required, Validators.min(0)]]
+  })
+
+
+ }
